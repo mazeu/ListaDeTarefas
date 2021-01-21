bf6da3bae501b853760d3080f61922099c7b2cb2
mazeu <marciomazeu@hotmail.com>
2021-01-20 22:12:37 -0300

    Modelo tarefa

--- tarefa.model.php
+++ tarefa.model.php
@@ -4,6 +4,14 @@ class Tarefa {
     private $id_status;
     private $tarefa;
     private $data_cadastro;
+
+    public function __get($atributo) {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+    }
 }
 
 
